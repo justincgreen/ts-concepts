3a4196c3695752473fd1bd61699e3d9236b03465
Justin Green <justingreendev@gmail.com>
2024-09-23 17:21:23 -0500

    arrays

---
+++ src/arrays.ts
@@ -0,0 +1,11 @@
+// Array of numbers
+// Two different ways to declare the same thing, an array of numbers
+const numArray: Array<number> = [1,2,3];
+// const numArray: number[] = [1,2,3];
+
+// Array of string
+// Two different ways to declare the same thing, an array of strings
+const strArray: Array<string> = ['H','i','!'];
+// const strArray: string[] = ['H', 'I', '!'];
+
+console.log(strArray);
